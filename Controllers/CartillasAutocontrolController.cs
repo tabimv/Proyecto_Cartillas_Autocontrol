6ed2bd615002f0e143485204f33cf1ea658567e6
tabimv <ta.melo@duocuc.cl>
2023-10-20 16:32:29 -0300

    Inicio de Desarrollo de Cartillas de Autocontrol

---
+++ Controllers/CartillasAutocontrolController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_Cartilla_Autocontrol.Models;
+
+
+namespace Proyecto_Cartilla_Autocontrol.Controllers
+{
+    public class CartillasAutocontrolController : Controller
+    {
+
+        private ObraManzanoConexion db = new ObraManzanoConexion();
+
+        public async Task<ActionResult> ListaCartillasPorActividad()
+        {
+            var detalleCartillas = db.DETALLE_CARTILLA.Include(d => d.ACTIVIDAD).Include(d => d.ITEM_VERIF).Where(d => d.ACTIVIDAD_actividad_id == d.ACTIVIDAD.actividad_id);
+            return View(await detalleCartillas.ToListAsync());
+        }
+        public async Task<ActionResult> VerCartilla(int id)
+        {
+            // 1. Obtener la actividad específica a partir del parámetro id
+            var actividad = await db.ACTIVIDAD.SingleOrDefaultAsync(a => a.actividad_id == id);
+
+            if (actividad == null)
+            {
+                return HttpNotFound(); // Otra acción adecuada en caso de que la actividad no se encuentre.
+            }
+
+            // 2. Obtener todos los elementos de verificación relacionados con esa actividad
+            var elementosVerificacion = await db.DETALLE_CARTILLA
+                .Include(dc => dc.ITEM_VERIF)
+                .Where(dc => dc.ACTIVIDAD_actividad_id == actividad.actividad_id)
+                .ToListAsync(); // Utiliza ToListAsync() para cargar los datos de la base de datos de forma asincrónica.
+
+            // 3. Pasar estos datos a la vista
+            ViewBag.Actividad = actividad; // Esto es opcional, pero te permite acceder a los datos de la actividad en la vista.
+            return View(elementosVerificacion);
+        }
+
+    }
+}
---
+++ Controllers/ItemVerifController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_Cartilla_Autocontrol.Models;
+
+namespace Proyecto_Cartilla_Autocontrol.Controllers
+{
+    public class ItemVerifController : Controller
+    {
+        private ObraManzanoConexion db = new ObraManzanoConexion();
+
+        // GET: ItemVerif
+        public async Task<ActionResult> Index()
+        {
+            var iTEM_VERIF = db.ITEM_VERIF.Include(i => i.ACTIVIDAD);
+            return View(await iTEM_VERIF.ToListAsync());
+        }
+
+        // GET: ItemVerif/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ITEM_VERIF iTEM_VERIF = await db.ITEM_VERIF.FindAsync(id);
+            if (iTEM_VERIF == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iTEM_VERIF);
+        }
+
+        // GET: ItemVerif/Create
+        public ActionResult Create()
+        {
+            ViewBag.ACTIVIDAD_actividad_id = new SelectList(db.ACTIVIDAD, "actividad_id", "codigo_actividad");
+            ViewBag.ACTIVIDAD_nombre_actividad = new SelectList(db.ACTIVIDAD, "actividad_id", "nombre_actividad");
+            return View();
+        }
+
+        // POST: ItemVerif/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "item_verif_id,elemento_verificacion,label,ACTIVIDAD_actividad_id")] ITEM_VERIF iTEM_VERIF)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ITEM_VERIF.Add(iTEM_VERIF);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ACTIVIDAD_actividad_id = new SelectList(db.ACTIVIDAD, "actividad_id", "codigo_actividad", iTEM_VERIF.ACTIVIDAD_actividad_id);
+            return View(iTEM_VERIF);
+        }
+
+        // GET: ItemVerif/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ITEM_VERIF iTEM_VERIF = await db.ITEM_VERIF.FindAsync(id);
+            if (iTEM_VERIF == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ACTIVIDAD_actividad_id = new SelectList(db.ACTIVIDAD, "actividad_id", "codigo_actividad", iTEM_VERIF.ACTIVIDAD_actividad_id);
+            return View(iTEM_VERIF);
+        }
+
+        // POST: ItemVerif/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "item_verif_id,elemento_verificacion,label,ACTIVIDAD_actividad_id")] ITEM_VERIF iTEM_VERIF)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(iTEM_VERIF).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ACTIVIDAD_actividad_id = new SelectList(db.ACTIVIDAD, "actividad_id", "codigo_actividad", iTEM_VERIF.ACTIVIDAD_actividad_id);
+            return View(iTEM_VERIF);
+        }
+
+        // GET: ItemVerif/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ITEM_VERIF iTEM_VERIF = await db.ITEM_VERIF.FindAsync(id);
+            if (iTEM_VERIF == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iTEM_VERIF);
+        }
+
+        // POST: ItemVerif/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ITEM_VERIF iTEM_VERIF = await db.ITEM_VERIF.FindAsync(id);
+            db.ITEM_VERIF.Remove(iTEM_VERIF);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
