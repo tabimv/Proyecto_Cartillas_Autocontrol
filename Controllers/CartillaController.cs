73a0818fe8937ac997a262b0447b9c05740568f5
tabimv <ta.melo@duocuc.cl>
2023-11-23 12:48:13 -0300

    Validaciones  y Proceso de Gestión de Cartilla de Autocontrol

--- Controllers/CartillaController.cs
+++ Controllers/CartillaController.cs
@@ -58,6 +58,7 @@ namespace Proyecto_Cartilla_Autocontrol.Controllers
                     using (var dbContext = new ObraManzanoNoviembre())  // Reemplaza 'TuDbContext' con el nombre de tu contexto de base de datos
                     {
                         // Guardar la CARTILLA
+                        viewModel.Cartilla.fecha = DateTime.Now;
                         dbContext.CARTILLA.Add(viewModel.Cartilla);
                         dbContext.SaveChanges();
 
@@ -138,27 +139,111 @@ namespace Proyecto_Cartilla_Autocontrol.Controllers
             return Json(jsonData, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult GetCombinacionesElementosInmuebles()
+         public JsonResult GetCombinacionesElementosInmuebles(int actividadId)
         {
-            List<dynamic> result = new List<dynamic>();
-
-            using (var context = new ObraManzanoNoviembre()) // Reemplaza 'TuContextoDeBaseDeDatos' con tu contexto de Entity Framework
+            try
             {
-                var query = from iv in context.ITEM_VERIF
-                            from i in context.INMUEBLE
-                            select new
-                            {
-                                iv.item_verif_id,
-                                iv.elemento_verificacion,
-                                iv.label,
-                                i.inmueble_id,
-                                i.tipo_inmueble
-                            };
+                using (var context = new ObraManzanoNoviembre())
+                {
+                    var query = from iv in context.ITEM_VERIF
+                                from i in context.INMUEBLE
+                                where iv.ACTIVIDAD_actividad_id == actividadId
+                                select new
+                                {
+                                    iv.item_verif_id,
+                                    iv.elemento_verificacion,
+                                    iv.label,
+                                    i.inmueble_id,
+                                    i.tipo_inmueble
+                                };
 
-                result = query.ToList<dynamic>();
+                    var result = query.ToList();
+                    return Json(result, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { error = "Error al obtener combinaciones: " + ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        public ActionResult EditarCartilla(int id)
+        {
+            // Obtener la Cartilla que se quiere editar por su ID
+            var cartilla = db.CARTILLA.FirstOrDefault(c => c.cartilla_id == id);
+
+            if (cartilla != null)
+            {
+                CartillasViewModel viewModel = new CartillasViewModel();
+                viewModel.Cartilla = cartilla;
+
+                // Obtener los detalles de la Cartilla que se quiere editar
+                viewModel.DetalleCartillas = db.DETALLE_CARTILLA.Where(d => d.CARTILLA_cartilla_id == id).ToList();
+
+                // Obtener otras listas necesarias para el formulario (actividades, elementos de verificación, inmuebles, etc.)
+                viewModel.ActividadesList = db.ACTIVIDAD.ToList();
+                viewModel.ElementosVerificacion = db.ITEM_VERIF.ToList();
+                viewModel.InmuebleList = db.INMUEBLE.ToList();
+                viewModel.EstadoFinalList = db.ESTADO_FINAL.ToList();
+
+                return View(viewModel);
             }
 
-            return Json(result, JsonRequestBehavior.AllowGet);
+            // Manejar si no se encuentra la Cartilla con el ID proporcionado
+            return RedirectToAction("Index", "Cartilla"); // O alguna otra acción adecuada
+        }
+
+        [HttpPost]
+        public ActionResult EditarCartilla(CartillasViewModel viewModel, List<DETALLE_CARTILLA> DetalleCartillas)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (var dbContext = new ObraManzanoNoviembre())
+                    {
+                        // Actualizar la información de la Cartilla en la base de datos
+                        dbContext.Entry(viewModel.Cartilla).State = EntityState.Modified;
+
+                        // Actualizar o agregar los detalles de la Cartilla en la base de datos
+                        foreach (var detalleCartilla in viewModel.DetalleCartillas)
+                        {
+                            if (detalleCartilla.detalle_cartilla_id == 0)
+                            {
+                                // Nuevo detalle de Cartilla, agregarlo
+                                detalleCartilla.CARTILLA_cartilla_id = viewModel.Cartilla.cartilla_id;
+                                dbContext.DETALLE_CARTILLA.Add(detalleCartilla);
+                            }
+                            else
+                            {
+                                // Detalle de Cartilla existente, marcar como modificado
+                                dbContext.Entry(detalleCartilla).State = EntityState.Modified;
+                            }
+                        }
+
+                        // Eliminar detalles de la Cartilla que se hayan quitado en la edición
+                        foreach (var detalle in dbContext.DETALLE_CARTILLA.Where(d => d.CARTILLA_cartilla_id == viewModel.Cartilla.cartilla_id))
+                        {
+                            if (!viewModel.DetalleCartillas.Any(d => d.detalle_cartilla_id == detalle.detalle_cartilla_id))
+                            {
+                                dbContext.DETALLE_CARTILLA.Remove(detalle);
+                            }
+                        }
+
+                        // Guardar los cambios en la base de datos
+                        dbContext.SaveChanges();
+
+                        return RedirectToAction("Index", "Cartilla");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Error al guardar en la base de datos: " + ex.Message);
+                }
+            }
+
+            return View(viewModel);
         }
 
 
